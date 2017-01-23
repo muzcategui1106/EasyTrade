327805613050405577a46acf580a7bc3b7acc1e7
muzcategui1106 <muzcategui1106@gmail.com>
2017-01-23 09:24:29 -0500

    Spring setup for EasyTradeWeb

---
+++ EasyTradeWeb/src/main/java/com/easytrade/easytradeweb/EasyTradeWebApplication.java
@@ -0,0 +1,12 @@
+package com.easytrade.easytradeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTradeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTradeWebApplication.class, args);
+	}
+}
---
+++ EasyTradeWeb/src/main/java/com/easytrade/easytradeweb/configuration/EasyTradeWebConfiguration.java
@@ -0,0 +1,38 @@
+package com.easytrade.easytradeweb.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by Sofy on 1/22/2017.
+ */
+@Configuration
+@EnableWebMvc
+public class EasyTradeWebConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/META-INF/resources/webjars/").setCachePeriod(31556926);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
